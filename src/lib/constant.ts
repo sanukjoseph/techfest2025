f4f829061a1921851c09c54504156ecacf8f97eb
sanu k joseph <sanukjoseph10101@gmail.com>
2025-02-27 09:12:41 +0530

    updated to add verificatin auth cred

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -2,4 +2,4 @@ export const ADMIN_EMAIL = "admin@smash.com";
 export const EVENTS_EMAIL = "event@smash.com";
 export const FACULTY_EMAIL = "faculty@smash.com";
 
-export const VERIFICATION_EMAILS = ["verify@gmail.com"];
+export const VERIFICATION_EMAILS = ["verify@gmail.com", "verify@smash.com"];
